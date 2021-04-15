41c0ed88f1d8c126d3f85098ae8922006f897fe2
vishakha <vishakhatyagi030@gmail.com>
2021-04-16 01:34:33 +0530

    package

---
+++ lec4_WebSc/facts/fileRead.js
@@ -0,0 +1,8 @@
+const fs=require("fs");
+let htmlkadata=fs.readFileSync("./index.html");
+console.log(htmlkadata+" ");
+
+//html ka data html treat ho
+//html me se=> selector ka base => element get
+
+ 
